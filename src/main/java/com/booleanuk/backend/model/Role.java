e73da8796ef3336e86638c7c30dbd4ececf36790
Alexander Gatland <alexander.gatland@no.experis.com>
2024-03-15 15:47:37 +0100

    Create Role Model

---
+++ src/main/java/com/booleanuk/backend/model/Role.java
@@ -0,0 +1,24 @@
+package com.booleanuk.backend.model;
+
+import com.booleanuk.backend.model.enums.ERole;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
