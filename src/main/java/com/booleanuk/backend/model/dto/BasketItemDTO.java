bf5d3a2bb01b4afb04f078ac8b329a2cdedf6243
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-18 16:00:43 +0100

    Create Basket Endpoints closes #20

---
+++ src/main/java/com/booleanuk/backend/model/dto/BasketItemDTO.java
@@ -0,0 +1,15 @@
+package com.booleanuk.backend.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasketItemDTO {
+    private int sku;
+    private int quantity;
+}
