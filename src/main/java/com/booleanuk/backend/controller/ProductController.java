91081247f0d135cd03769daf7d889eb3e0fe34d6
Alexander Gatland <alexander.gatland@no.experis.com>
2024-03-18 10:18:39 +0100

    Create getSpecific endpoint

--- src/main/java/com/booleanuk/backend/controller/ProductController.java
+++ src/main/java/com/booleanuk/backend/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.booleanuk.backend.controller;
 
+import com.booleanuk.backend.model.Product;
+import com.booleanuk.backend.payload.response.ErrorResponse;
 import com.booleanuk.backend.payload.response.ProductListResponse;
+import com.booleanuk.backend.payload.response.ProductResponse;
 import com.booleanuk.backend.payload.response.Response;
 import com.booleanuk.backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,4 +24,16 @@ public class ProductController {
         return ResponseEntity.ok(productListResponse);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Response<?>> getSpecific(@PathVariable int id) {
+        Product product = this.productRepository.findById(id).orElse(null);
+        if (product == null) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("Product Not Found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.set(product);
+        return ResponseEntity.ok(productResponse);
+    }
 }
