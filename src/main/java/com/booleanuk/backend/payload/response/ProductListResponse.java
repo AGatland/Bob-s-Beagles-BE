023cd5b4679450495a448f4294cd03f26ab94eae
Alexander Gatland <alexander.gatland@no.experis.com>
2024-03-18 10:17:01 +0100

    Create Response classes for product

---
+++ src/main/java/com/booleanuk/backend/payload/response/ProductListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.backend.payload.response;
+
+import com.booleanuk.backend.model.Product;
+
+import java.util.List;
+
+public class ProductListResponse extends Response<List<Product>> {
+}
