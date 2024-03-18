50b2c2128a07c8b2ca8f9549593f290a1f44abce
Alexander Gatland <alexander.gatland@no.experis.com>
2024-03-18 10:18:13 +0100

    Create getAll endpoint

--- src/main/java/com/booleanuk/backend/controller/ProductController.java
+++ src/main/java/com/booleanuk/backend/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.booleanuk.backend.controller;
 
+import com.booleanuk.backend.payload.response.ProductListResponse;
+import com.booleanuk.backend.payload.response.Response;
 import com.booleanuk.backend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +13,11 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @GetMapping
+    public ResponseEntity<Response<?>> getAll() {
+        ProductListResponse productListResponse = new ProductListResponse();
+        productListResponse.set(this.productRepository.findAll());
+        return ResponseEntity.ok(productListResponse);
+    }
 
 }
