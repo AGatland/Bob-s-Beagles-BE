03b95445e3cfd256b40e49835b89f4b300bbf5a1
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-19 11:41:55 +0100

    Create Order Endpoints closes #15

---
+++ src/main/java/com/booleanuk/backend/payload/response/OrderListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.backend.payload.response;
+
+import com.booleanuk.backend.model.dto.OrderDTO;
+
+import java.util.List;
+
+public class OrderListResponse extends Response<List<OrderDTO>> {
+}
